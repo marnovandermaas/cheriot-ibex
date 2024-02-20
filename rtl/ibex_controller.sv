1ed8e95207008d5a36ca2f28b213f9d017b835db
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-02-20 14:48:47 +0000

    Fix width assignment of `csr_mtval_o` in control
    These cause width lint errors from Verilator

--- rtl/ibex_controller.sv
+++ rtl/ibex_controller.sv
@@ -780,7 +780,7 @@ module ibex_controller #(
                   csr_mtval_o = lsu_addr_last_i;
                 end else begin
                   exc_cause_o = EXC_CAUSE_CHERI_FAULT; 
-                  csr_mtval_o = cheri_wb_err_info_i[10:0];
+                  csr_mtval_o = {21'h0, cheri_wb_err_info_i[10:0]};
                 end
               end else begin
                 exc_cause_o = EXC_CAUSE_STORE_ACCESS_FAULT;
@@ -794,7 +794,7 @@ module ibex_controller #(
                   csr_mtval_o = lsu_addr_last_i;
                 end else begin
                   exc_cause_o = EXC_CAUSE_CHERI_FAULT;
-                  csr_mtval_o = cheri_wb_err_info_i[10:0];
+                  csr_mtval_o = {21'h0, cheri_wb_err_info_i[10:0]};
                 end
               end else begin
                 exc_cause_o = EXC_CAUSE_LOAD_ACCESS_FAULT;
@@ -804,13 +804,13 @@ module ibex_controller #(
             cheri_ex_err_prio: begin
               if (cheri_pmode_i) begin
                 exc_cause_o = EXC_CAUSE_CHERI_FAULT;
-                csr_mtval_o = cheri_ex_err_info_i[10:0];        
+                csr_mtval_o = {21'h0, cheri_ex_err_info_i[10:0]};
               end
             end
             cheri_wb_err_prio: begin
               if (cheri_pmode_i) begin
                 exc_cause_o = EXC_CAUSE_CHERI_FAULT;
-                csr_mtval_o = cheri_wb_err_info_i[10:0];
+                csr_mtval_o = {21'h0, cheri_wb_err_info_i[10:0]};
               end
             end
 
